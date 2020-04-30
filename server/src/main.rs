31af8e3b58a2ea531f23b64c4673cac7cd76fb05
oScape <benoit.chassignol@gmail.com>
2020-04-30 23:44:05 +0200

    switch to api

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,4 @@
 use server_lib::ThreadPool;
-use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -10,7 +9,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let _stream = stream.unwrap();
         pool.execute(|| {
             handle_connection(_stream);
@@ -26,17 +25,19 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
+    let data = b"GET /data HTTP/1.1\r\n";
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    let (status_line, contents) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "data")
+    } else if buffer.starts_with(data) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "sleep")
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "not found")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
     let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
